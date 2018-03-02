1a2546269eede1db50629688d7b99d13926256a9
Roxanne J Carini <rjcarini@uw.edu>
2018-03-02 13:13:50 -0800

    Updates
    Added documentation.

--- detrendIR_detectBrkers.m
+++ detrendIR_detectBrkers.m
@@ -1,27 +1,46 @@
-% Load IR images into array I (rows x cols x frames):
-load('sampleImageSeries.mat')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Script to detrend Thermal Infrared images and isolate breaking waves.
+%
+% Created on 2 March 2018 by Roxanne J Carini at Applied Physics
+% Laboratory, University of Washington.
+% Contact: rjcarini@uw.edu
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Load data
+
+% Load IR images into array I (#rows x #cols x #frames):
+% This file available in matlab_SampleData repository.
+path = '/Users/rjcarini/Documents/GitHub/matlab_SampleData/';
+load([path,'sampleImageSeries.mat'])
+
+
+%% Calculate trend or drift of pixel intensity over time
 
 % Choose reference region centered on your area of interest for use in
 % thresholding algorithm:
-ref = I(310:339,210:239,:);
-ref = reshape(ref,[size(ref,1)*size(ref,2),size(ref,3)]);
+ref3d = I(310:339,210:239,:);
+ref2d = reshape(ref3d,[size(ref3d,1)*size(ref3d,2),size(ref3d,3)]);
 
 % Use detrend the data through two median-smoothing operations:
-reftrend = nanmedian(ref);
+reftrend = nanmedian(ref2d);
 smoothparam = round(numel(reftrend)/10);
 reftrendsmooth = movmedian(reftrend,smoothparam);
 reftrendsmooth = movmedian(reftrendsmooth,smoothparam); 
+reftrendsmooth = reshape(reftrendsmooth,1,1,length(reftrendsmooth));
+refdetrend = ref3d-repmat(reftrendsmooth,size(ref3d,1),size(ref3d,2));
 reftrendmat = repmat(reftrendsmooth,size(I,1),size(I,2));
-refdetrend = ref-reftrendmat;
 
 % Visualize trends:
 figure('position',[50 50 800 300])
 hold on
 for i=1:length(timevec)
-    p0 = plot(repmat(timevec(i),size(ref,1),1),ref(:,i),'.k');
+    p0 = plot(repmat(timevec(i),size(ref2d,1),1),ref2d(:,i),'.k');
 end
 p1 = plot(timevec,reftrend,'-r');
-p2 = plot(timevec,reftrendsmooth,'-c');
+p2 = plot(timevec,squeeze(reftrendsmooth),'-c');
 axis([timevec(1) timevec(end) 2e4 2.25e4])
 grid on
 ylabel('Pixel Intensity')
@@ -29,9 +48,11 @@ xlabel('Time')
 datetick('x','keepticks','keeplimits')
 legend([p0,p1,p2],'All reference data','Median at each time step','Smoothed reference level')
 
-% Apply thresholding algorithm to raw images:
-[Iraw,brkMask] = detectBreakers_singlecam(I,ref);
 
+%% Run detection algorithm on raw and detrended pixel intensities
+
+% Apply thresholding algorithm to raw images:
+[Iraw,brkMask] = detectBreakers_singlecam(I,ref2d);
 
 % Apply thresholding algorithm to detrended images:
 Idetrend = I-reftrendmat;
